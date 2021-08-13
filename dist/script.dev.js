c9a99a65ea6906eef40f06a72beae9df3d095067
valnicky <valery0007@gmail.com>
2021-08-13 11:12:33 +0200

    btn js

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -19,20 +19,8 @@ var emptyStateDiv = todolist.querySelector('.todolist__empty-state');
 var promises = zlFetch("".concat(rootendpoint, "/tasks"), {
   auth: auth
 });
-promises.then(function (response) {
-  var tasks = response.body; //append tasks to DOM
-
-  tasks.forEach(function (task) {
-    var taskElement = makeTaskElement1(task);
-    todolist.appendChild(taskElement);
-  });
-  var todolistbtn = document.querySelector('.container__btn');
-  todolistbtn.addEventListener('submit', addtasktodom); //change empty state text
-
-  emptyStateDiv.textContent = 'Your todo list is empty!  🎉';
-})["catch"](function (error) {
-  return console.error(error);
-}); // .then(response => console.log(response.body))
+var todolistbtn = document.querySelector('.container__btn');
+todolistbtn.addEventListener('submit', addtasktodom); // .then(response => console.log(response.body))
 
 function makeTaskElement(elem) {
   var li = document.createElement('li');
@@ -47,20 +35,26 @@ var makeTaskElement1 = function makeTaskElement1(_ref) {
       done = _ref.done;
   var taskElement = document.createElement('li');
   taskElement.classList.add('task');
-  taskElement.innerHTML = "\n    <input type=\"checkbox\" id=\"".concat(id, "/>\"\n    <label for=\"").concat(id, "\">...</label>\n    <span class=\"task__name\">").concat(name, "</span>\n    <button type=\"button\" class=\"task__delete-button\">\n    <svg viewBox=\"0 0 20 20\">... </svg>\n    </button>");
+  taskElement.innerHTML = "\n    <input type=\"checkbox\" id=\"".concat(id, "\"/>\n    <label for=\"").concat(id, "\">...</label>\n    <span class=\"task__name\">").concat(name, "</span>\n    <button type=\"button\" class=\"task__delete-button\">\n    <svg viewBox=\"0 0 20 20\">... </svg>\n    </button>");
   return taskElement;
 };
 
 function addtasktodom(event) {
-  event.preventDefault();
+  event.preventDefault(); //get value of task
+
   var newTaskField = todolist.querySelector('input');
-  var inputValue = DOMPurify.sanitize(newTaskField.value.trim());
+  var inputValue = newTaskField.value.trim();
+  console.log("input value " + inputValue);
   var id = generateUniqueString(10);
-  /*   const taskElement = makeTaskElement(
-         id,
-         name: inputValue,
-         done: false
-     );*/
+  var taskElement = makeTaskElement1({
+    id: id,
+    name: inputValue,
+    done: false
+  }); //prevent adding of empty task
+
+  if (!inputValue) return; //give indication that we're adding a task
+
+  todolistbtn.textContent = 'Adding task...'; //sends POST request...
 
   zlFetch.post("".concat(rootendpoint, "/tasks"), {
     auth: auth,
@@ -69,6 +63,32 @@ function addtasktodom(event) {
       name: DOMPurify.sanitize(inputValue)
     }
   }).then(function (response) {
-    console.log(response.body);
+    console.log(response.body); //append task to DOM
+
+    var tasks = response.body; //append tasks to DOM
+
+    tasks.forEach(function (task) {
+      var taskElement = makeTaskElement1(task);
+      todolist.appendChild(taskElement);
+    });
+  })["catch"](function (error) {
+    return console.error(error);
+  })["finally"](function (_) {
+    //change button text back to original text 
+    todolistbtn.textContent = 'Add task';
+  });
+  promises.then(function (response) {
+    //append task to DOM
+    var task = response.body;
+    var taskElement = makeTaskElement1(task);
+    todolist.appendChild(taskElement); //clear the new task field
+
+    newTaskField.value = ''; //bring focus back to input field
+
+    newTaskField.focus(); //change empty state text
+
+    emptyStateDiv.textContent = 'Your todo list is empty!  🎉';
+  })["catch"](function (error) {
+    return console.error(error);
   });
 }
--- script.js
+++ script.js
@@ -16,23 +16,10 @@ const auth = {
 const todolist = document.querySelector('body .container .container__list');
 const emptyStateDiv = todolist.querySelector('.todolist__empty-state');
 const promises = zlFetch(`${rootendpoint}/tasks`, { auth });
+const todolistbtn = document.querySelector('.container__btn');
 
-promises.then(response => {
-        const tasks = response.body;
-        //append tasks to DOM
-        tasks.forEach(task => {
-            const taskElement = makeTaskElement1(task);
+todolistbtn.addEventListener('submit', addtasktodom);
 
-            todolist.appendChild(taskElement);
-        })
-
-        const todolistbtn = document.querySelector('.container__btn');
-        todolistbtn.addEventListener('submit', addtasktodom);
-
-        //change empty state text
-        emptyStateDiv.textContent = 'Your todo list is empty!  🎉'
-    })
-    .catch(error => console.error(error));
 
 // .then(response => console.log(response.body))
 
@@ -47,7 +34,7 @@ const makeTaskElement1 = ({ id, name, done }) => {
     const taskElement = document.createElement('li');
     taskElement.classList.add('task');
     taskElement.innerHTML = (`
-    <input type="checkbox" id="${id}/>"
+    <input type="checkbox" id="${id}"/>
     <label for="${id}">...</label>
     <span class="task__name">${name}</span>
     <button type="button" class="task__delete-button">
@@ -59,24 +46,65 @@ const makeTaskElement1 = ({ id, name, done }) => {
 function addtasktodom(event) {
     event.preventDefault()
 
+    //get value of task
     const newTaskField = todolist.querySelector('input');
-    const inputValue = DOMPurify.sanitize(newTaskField.value.trim());
+    const inputValue = newTaskField.value.trim();
+    console.log("input value " + inputValue);
 
     const id = generateUniqueString(10);
-    /*   const taskElement = makeTaskElement(
-           id,
-           name: inputValue,
-           done: false
-       );*/
+    const taskElement = makeTaskElement1({
+        id,
+        name: inputValue,
+        done: false
+    });
 
+    //prevent adding of empty task
+    if (!inputValue) return;
 
+//disable btn
+todolistbtn.setAttribute('disabled', true);
+    //give indication that we're adding a task
+    todolistbtn.textContent = 'Adding task...';
+
+    //sends POST request...
     zlFetch.post(`${rootendpoint}/tasks`, {
-        auth,
-        body: {
-            // Information about the task
-            name: DOMPurify.sanitize(inputValue)
-        }
-    }).then(response => {
-        console.log(response.body);
-    })
+            auth,
+            body: {
+                // Information about the task
+                name: DOMPurify.sanitize(inputValue)
+            }
+        }).then(response => {
+            console.log(response.body);
+            //append task to DOM
+            const tasks = response.body;
+            //append tasks to DOM
+            tasks.forEach(task => {
+                const taskElement = makeTaskElement1(task);
+
+                todolist.appendChild(taskElement);
+            })
+        })
+        .catch(error => console.error(error))
+        .finally(_ => {
+            //enables btn 
+            todolistbtn.removeAttribute(''disabled);
+            //change button text back to original text 
+            todolistbtn.textContent = 'Add task';
+        })
+
+    promises.then(response => {
+            //append task to DOM
+            const task = response.body;
+            const taskElement = makeTaskElement1(task);
+            todolist.appendChild(taskElement);
+
+            //clear the new task field
+            newTaskField.value = '';
+
+            //bring focus back to input field
+            newTaskField.focus();
+            //change empty state text
+            emptyStateDiv.textContent = 'Your todo list is empty!  🎉'
+        })
+        .catch(error => console.error(error));
 }
